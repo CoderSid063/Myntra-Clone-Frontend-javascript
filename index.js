a472c7d1c7f4ba25c62ae00402cd1179cc021bb0
CoderSid063 <sudhansupanda063@gmail.in>
2023-11-02 08:34:06 +0530

    Update js File

---
+++ index.js
@@ -0,0 +1,47 @@
+let bagItem = [];
+onLoad();
+
+function onLoad(){
+   displayItemsOnHomePage();
+   displayBagItem();
+};
+
+
+function addToBag (itemId){
+   bagItem.push(itemId);
+   displayBagItem();
+};
+
+function displayBagItem () {
+   let bagItemCountElement = document.querySelector('.bag-item-count');
+   if(bagItem.length > 0){
+      bagItemCountElement.style.visibility = 'visible';
+      bagItemCountElement.innerText = bagItem.length;
+   } else {
+      bagItemCountElement.style.visibility = 'hidden';
+   }
+};
+
+function displayItemsOnHomePage () {
+   let itemsContainerElement = document.querySelector('.items-container');
+
+   let innerHtml ='';
+   items.forEach((item) => {
+      innerHtml += `
+      <div class="item-container">
+         <img class="item-image" src=${item.image} alt="item-image">
+         <div class="rating">
+            ${item.rating.stars} ⭐ | ${item.rating.count}
+         </div>
+         <div class="company-name">${item.company}</div>
+         <div class="item-name">${item.item_name}</div>
+         <div class="price">
+            <span class="current-price">Rs ${item.current_price}</span>
+            <span class="original-price">Rs ${item.original_price}</span>
+            <span class="discount">(${item.discount_percentage}% OFF)</span>
+         </div>
+         <button class="btn-add-bag" onclick = "addToBag(${item.id})";>Add to Bag</button>
+      </div>`;
+});
+itemsContainerElement.innerHTML = innerHtml;
+};
